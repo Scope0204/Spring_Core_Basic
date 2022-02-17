b2d0ac71718189d465acf521b75dedd104baf370
Scope0204 <scope0204@gmail.com>
2022-02-17 13:23:12 +0900

    DI 외부관계 주입

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,10 +1,21 @@
-package hello.core.member;
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
 
     //psvm
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+
+        // 이전에는 직접 메인메서드에서 MemberService 생성
+        //MemberService memberService = new MemberServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+
         // command + option + v
         Member member = new Member(1L, "memberA", Grade.VIP); // id가 long타입이므로 뒤에 L을 붙여줘야 함
         memberService.join(member);
