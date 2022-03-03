880a7fe7b5657b30aa7beb6d420556e53f81a2c8
Scope0204 <scope0204@gmail.com>
2022-03-03 13:15:26 +0900

    request scope +  ObjectProvider

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,32 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+
+import hello.core.logdemo.LogDemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor // Lombok의 기능 중 하나 : final을 파라미터로 받는 생성자를 만들어줌
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testID");
+        return "OK";
+
+    }
+}
